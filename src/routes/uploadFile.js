1e25fdfcbc78531b36b6989f6c68aca9651ccc42
Simmo-tech <school12p@gmail.com>
2024-05-20 00:17:18 +0300

    last

--- src/routes/uploadFile.js
+++ src/routes/uploadFile.js
@@ -1,29 +1,99 @@
 const express = require("express");
 const router = express.Router();
-const {
-  UpdateAvatarFiles,
-  UploadAvatarFiles,
-} = require("../controllers/uploadAvatar");
-const {
-  UpdatePortfolioFiles,
-  UploadPortfolioFiles,
-} = require("../controllers/uploadPortfolio");
 const { VerifyUser } = require("../middleware/verifyUser");
+const multer = require("multer");
+const asyncHandler = require("express-async-handler");
+const { UserSc } = require("../models/UserSchema");
+const cloudinary = require("cloudinary").v2;
 
-// avatar upload && update image
+// file filter
+const maxSize = 2 * 1024 * 1024; // 2 MB
+const storage = multer.diskStorage({
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: maxSize },
+  fileFilter: (req, file, cb) => {
+    const allowedTypes = ["image/jpeg", "image/png", "image/jpg", "image/webp"];
+    if (!allowedTypes.includes(file.mimetype)) {
+      return cb("file type not supported", false);
+    }
+    cb(null, true);
+  },
+});
+// cloudinary
+// Cloudinary configuration
+cloudinary.config({
+  cloud_name: process.env.NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+});
+
+/**
+ * @desc upload avatar photo
+ * @route /avatar/:id"
+ * @method PUT
+ * @access public
+ */
 router.put(
   "/avatar/:id",
   VerifyUser,
-  UploadAvatarFiles.single("file"),
-  UpdateAvatarFiles
-);
+  upload.single("file"),
+  asyncHandler(async (req, res) => {
+    const date = new Date().getHours() + new Date().getSeconds();
+    const file = req.file;
 
-// portfolio upload && update image
+    if (!file) {
+      res.status(400).send({ success: false, message: "no file choose" });
+    }
+
+    // Upload an image
+    const img = await cloudinary.uploader.upload(file.path);
+    console.log(img.secure_url);
+
+    let user = await UserSc.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          avatar: img.secure_url,
+        },
+      },
+      { new: true, runValidators: true }
+    );
+
+    res.status(200).send({
+      avatar: user.avatar,
+      success: true,
+      message: "upload successfully",
+    });
+  })
+);
+/**
+ * @desc upload portfolio photo
+ * @route /portfolio-imgs/:id"
+ * @method PUT
+ * @access public
+ */
 router.put(
-  "/portfolioImg/:id",
+  "/portfolio-imgs/:id",
   VerifyUser,
-  UploadPortfolioFiles.single("file"),
-  UpdatePortfolioFiles
+  upload.single("file"),
+  asyncHandler(async (req, res) => {
+    const file = req.file;
+    if (!file) {
+      res.status(400).send({ success: false, message: "no file choose" });
+    }
+    // Upload an image
+    const img = await cloudinary.uploader.upload(file.path);
+    res.status(200).send({
+      path: img.secure_url,
+      success: true,
+      message: "upload successfully",
+    });
+  })
 );
 
 module.exports = router;
