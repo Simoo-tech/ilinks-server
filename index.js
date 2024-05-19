ef225886151a62ae00ee7073f01c0f0e00db632d
Simmo-tech <school12p@gmail.com>
2024-05-19 10:58:37 +0300

    last

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(
   })
 );
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers");
+  res.header("Access-Control-Allow-Headers", "*");
 });
 
 // Routes
